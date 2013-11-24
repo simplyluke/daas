523e3fed79bd7317072941c79c6d58607e114538
Luke Wright <lukewright1997@gmail.com>
2013-11-24 16:49:43 -0600

    much json. many refactor

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
+
+def output(req, data):
+  if req.headers['Content-Type'] == 'application/json':
+    return jsonify(data)
+  elif req.headers['Content-Type'] == 'text/plain':
+    return data['main']
+  else:
+    return render_template('api.html', content = data)
+
 
 @app.route('/')
 def index():
@@ -13,43 +22,53 @@ def index():
 @app.route('/wow/<arg>')
 def show_wow(arg):
   main = 'wow such %s.' % arg
-  return render_template('api.html', content = {'title': 'wow', 'main': main })
+  content = {'title': 'wow', 'main': main}
+  return output(request, content)
 
 @app.route('/very/<arg>')
 def show_very(arg):
   main = 'very %s. wow.' % arg
-  return render_template('api.html', content={'title': 'very', 'main': main})
+  content={'title': 'very', 'main': main}
+  return output(request, content)
 
 @app.route('/little/<arg>')
 def show_little(arg):
   main = 'little %s. wow.' % arg
-  return render_template('api.html', content={'title': 'little', 'main': main})
+  content={'title': 'little', 'main': main}
+  return output(request, content)
+
 
 @app.route('/wat/<arg>')
 def show_wat(arg):
   main = 'wat r u doin, %s.' % arg
-  return render_template('api.html', content={'title': 'wat', 'main': main})
+  content={'title': 'wat', 'main': main}
+  return output(request, content)
+
 
 @app.route('/omg/<arg>')
 def show_omg(arg):
   main = 'omg such %s. wow.' % arg
-  return render_template('api.html', content={'title': 'omg', 'main': main})
+  content={'title': 'omg', 'main': main}
+  return output(request, content)
   
 # many variable
 @app.route('/plz/<to>/<sender>')
 def show_plz(to, sender):
   main = '%s plz' % to
-  return render_template('api.html', content={'title': 'plz', 'main': main, 'optional': sender })
+  content={'title': 'plz', 'main': main, 'optional': sender }
+  return output(request, content)
 
 @app.route('/thx/<to>/<sender>')
 def show_thx(to, sender):
   main = 'thx %s' % to
-  return render_template('api.html', content={'title': 'to', 'main': main, 'optional': sender})
+  content={'title': 'to', 'main': main, 'optional': sender}
+  return output(request, content)
 
 @app.route('/fuk/<to>/<sender>')
 def show_fuk(to, sender):
   main = 'fuk u %s' % to
-  return render_template('api.html', content={'title': 'fuk u', 'main': main, 'optional': sender }) 
+  content={'title': 'fuk u', 'main': main, 'optional': sender }
+  return output(request, content) 
 
 if __name__ == '__main__':
   app.run(debug=True)
