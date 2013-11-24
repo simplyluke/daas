d30cfa22faab5de3969cc59449c87aa6f0871b97
Luke Wright <lukewright1997@gmail.com>
2013-11-24 15:46:01 -0600

    plz work gunicorn

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 @app.route('/')
 def index():
