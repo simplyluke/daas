962d9b667c119d99045676e5a0cd85b5a616e5d8
Luke Wright <lukewright1997@gmail.com>
2013-11-24 14:20:36 -0600

    such unit tests

--- app-tests.py
+++ app-tests.py
@@ -4,6 +4,7 @@ import app
 
 class DaasTestCase(unittest.TestCase):
 
+  # Such unit testing 
   def setUp(self):
     self.app = app.app.test_client()
 
@@ -14,6 +15,34 @@ class DaasTestCase(unittest.TestCase):
   def test_wow(self):
     rv = self.app.get('/wow/foobar')
     assert 'wow such foobar' in rv.data
+
+  def test_very(self):
+    rv = self.app.get('/very/foobar')
+    assert 'very foobar. wow' in rv.data
+
+  def test_plz(self):
+    rv = self.app.get('/plz/foo/bar')
+    assert 'foo plz' and 'from bar' in rv.data
+
+  def test_little(self):
+    rv = self.app.get('/little/foo')
+    assert 'little foo. wow' in rv.data
+
+  def test_wat(self):
+    rv = self.app.get('/wat/foo')
+    assert 'wat r u doin, foo' in rv.data
+
+  def test_omg(self):
+    rv = self.app.get('/omg/foo')
+    assert 'omg such foo. wow' in rv.data
+
+  def test_thx(self):
+    rv = self.app.get('/thx/foo/bar')
+    assert 'thx foo' and 'from bar' in rv.data
+
+  def test_fuk(self):
+    rv = self.app.get('/fuk/foo/bar')
+    asser 'fuk u foo' and 'from bar' in rv.data
 
 
 if __name__ == '__main__': unittest.main()
